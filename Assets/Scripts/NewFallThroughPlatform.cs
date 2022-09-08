41611eaeaa0a7c9e3cfde9225c052183db6d66e9
AmazingProgramadora <anninha0307trabalhos@gmail.com>
2022-09-08 20:19:12 -0300

    tentativa de um novo script pra plataforma

---
+++ Assets/Scripts/NewFallThroughPlatform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewFallThroughPlatform : MonoBehaviour
+{
+    /* private Collider2D _collider;
+     void Start()
+     {
+         _collider = GetComponent<Collider2D>();
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }
+
+     void OnCollisionEnter(Collision2D collision)
+     {
+         if (collision.gameObject.tag == "Player")
+         {
+             Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+         }
+     }
+     */
+}
+//pq não está funcionando?
